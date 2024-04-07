3029cdd354d578522e8e4aece3ae6f2e98377a36
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-04-07 11:02:11 +0300

    validation rules added to Student

--- src/main/java/com/agan/springboot/model/Student.java
+++ src/main/java/com/agan/springboot/model/Student.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
@@ -14,11 +15,24 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+    @NotNull(message = "Must be not null")
+    @Size(min = 3)
     private String firstName;
+
+    @NotNull
+    @Size(min = 3)
     private String lastName;
+
+    @NotBlank(message = "Must be not blank")
     private String schoolName;
+
+    @Email(message = "Email should be valid")
     private String email;
+
+    @Min(value = 3, message = "Cannot be younger than 18 years old")
     private int age;
+
+    @Pattern(regexp = "[0-9\\s]{12}")
     private String phoneNumber;
 
 
