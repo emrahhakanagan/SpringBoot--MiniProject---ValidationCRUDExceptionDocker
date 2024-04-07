b452b46df5896e9f5a33ca4ed46abc0d2b80c92c
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-04-07 09:59:59 +0300

    initial commit

---
+++ src/main/java/com/agan/springboot/SpringBootValidationCrudExceptionDockerApplication.java
@@ -0,0 +1,13 @@
+package com.agan.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootValidationCrudExceptionDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootValidationCrudExceptionDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agan/springboot/SpringBootValidationCrudExceptionDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agan.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootValidationCrudExceptionDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
